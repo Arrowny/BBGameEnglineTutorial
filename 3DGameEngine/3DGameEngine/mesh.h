1960e2fe5dce56312ff6f4fc9df676d28872bd2e
David <davidklaus0@gmail.com>
2014-06-26 17:44:32 -0400

    Began working on Mesh class

--- 3DGameEngine/3DGameEngine/mesh.h
+++ 3DGameEngine/3DGameEngine/mesh.h
@@ -1,8 +1,47 @@
 #pragma once
+#include <GL\glew.h>
+#include <glm\glm.hpp>
+
+struct Vertex
+{
+public:
+	Vertex(const glm::vec3& pos, const glm::vec2& texCoord, const glm::vec3& normal = glm::vec3(0, 0, 0))
+	{
+		this->pos = pos;
+		this->texCoord = texCoord;
+		this->normal = normal;
+	}
+
+	inline glm::vec3* GetPos() { return &pos; }
+	inline glm::vec2* GetTexCoord() { return &texCoord; }
+	inline glm::vec3* GetNormal() { return &normal; }
+
+protected:
+private:
+	glm::vec3 pos;
+	glm::vec2 texCoord;
+	glm::vec3 normal;
+};
+
 class Mesh
 {
 public:
-	Mesh();
+	Mesh(Vertex* vertices, unsigned int numVertices);
 	virtual ~Mesh();
+
+	void Draw();
+
+	enum
+	{
+		POSITION_VB,
+
+		NUM_BUFFERS
+	};
+
+
+	GLuint m_vertexArrayObject;
+	GLuint m_vertexArrayBuffers[NUM_BUFFERS];
+
+	unsigned int m_drawCount;
 };
 
