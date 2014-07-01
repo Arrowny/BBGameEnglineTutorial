d1691ae1fe0eec9c1b1b9a36550a2e8aef0b673a
SherryShuranHuang <huangsr6@bu.edu>
2014-07-01 15:26:57 -0400

    hahaha

---
+++ 3DGameEngine/3DGameEngine/Vertex.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <glm/glm.hpp>
+
+class Vertex
+{
+public:
+	int SIZE = 3;
+	Vertex(const glm::vec3& pos)//, const glm::vec2& texCoord
+	{
+		this->pos = pos;
+		//this->texCoord = texCoord;
+	}
+
+	inline glm::vec3* GetPos() { return &pos; }
+	inline glm::vec3* SetPos(glm::fvec3& vertex) { this->pos = vertex; }
+
+	//inline glm::vec2* GetTexCoord() { return &texCoord; }
+
+private:
+	glm::vec3 pos;// variables: vertex attribute
+	//glm::vec2 texCoord;
+
+};
+
