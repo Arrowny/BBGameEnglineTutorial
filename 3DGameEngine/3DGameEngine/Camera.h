2b52ba87d4faf29159cfca9408a525eda2fd0846
SherryShuranHuang <huangsr6@bu.edu>
2014-07-03 11:25:36 -0400

    half way camera(quaternion

--- 3DGameEngine/3DGameEngine/Camera.h
+++ 3DGameEngine/3DGameEngine/Camera.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <glm/glm.hpp>
 #include <glm/gtx/transform.hpp>
+#include <glm/vec3.hpp>
 
 
 class Camera
@@ -13,12 +14,46 @@ public:
 		m_forward = glm::vec3(0, 0, 1);
 		m_up = glm::vec3(0, 1, 0);
 
+		glm::normalize(m_up);
+		glm::normalize(m_forward);
 	}
 
 	inline glm::mat4 GetViewProjection() const
 	{
 		return m_perspective * glm::lookAt(m_position, m_position + m_forward, m_up);
 	}
+
+	inline void move(glm::vec3 dir, float amt)
+	{
+		m_position = m_position + dir * amt;
+	}
+
+	inline void RotateX(float angle)
+	{
+		glm::vec3 Haxis = glm::cross(glm::vec3(0, 1, 0), m_forward);
+		glm::normalize(Haxis);
+
+		m_forward = glm::ro
+	}
+	inline glm::vec3 GetLeft()
+	{
+		glm::vec3 left = glm::cross(m_up, m_forward);
+		glm::normalize(left);
+		return left;
+	}
+
+	inline glm::vec3 GetRight()
+	{
+		glm::vec3 right = glm::cross(m_forward, m_up);
+		glm::normalize(right);
+		return right;
+	}
+
+	inline glm::vec3 GetPos()
+	{
+		return m_position;
+	}
+
 private:
 	glm::mat4 m_perspective;
 	glm::vec3 m_position;
