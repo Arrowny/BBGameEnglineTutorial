409e9f642c1cc0c937dd9debed32ef0f15192305
SherryShuranHuang <huangsr6@bu.edu>
2014-08-06 10:32:49 -0400

    change position

--- 3DGameEngine/3DGameEngine/testGame.cpp
+++ 3DGameEngine/3DGameEngine/testGame.cpp
@@ -120,7 +120,7 @@ void TestGame::init(){
 	m_camera->AddComponent(new Camera(70.0f, Window::getAspect(), 0.1f, 1000.0f))->AddComponent(new FreeLook())->AddComponent(new FreeMove());
 	m_skyBoxObject->AddComponent(m_skyBoxrenderer);
 
-	m_basicPS = new ParticleSystem("basicParticlePhy", &m_material4, glm::vec3(15,15,15));
+	m_basicPS = new ParticleSystem("basicParticlePhy", &m_material4, glm::vec3(5,5,5));
 	m_basicPSObj->AddComponent(m_basicPS);
 
 	AddToScene(m_planeObject);
